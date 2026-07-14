8559db35001e24992d74b778de08b79ac90f7583
Leonardo Oliveira <leo.obsidian@outlook.com>
2026-07-14 14:38:38 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ObsidianCapital from './ObsidianCapital.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ObsidianCapital />
+  </React.StrictMode>
+)
